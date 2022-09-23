30556c7ad4958572a95eb99472204358bbc6219e
iagoMachado96 <202204384833@alunos.estacio.br>
2022-09-23 17:54:58 -0300

    Aulas de rotas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,56 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function (){
+    return 'Página de contatos';
+});
+
+Route::get('/empresa', function (){
+    return view('site.empresa');
+});
+
+Route::any('/any', function (){
+    return 'Any';
+});
+
+Route::match(['get', 'post'], '/match', function (){
+    return 'Match';
+});
+
+Route::get('/contato/{flag}', function ($contato){
+    return "Contato via {$contato}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($categoria){
+    return "Posts da categoria: {$categoria}";
+});
+
+Route::get('/produtos/{idProduto?}', function ($idProduto = ''){
+    return "Produto(s): {$idProduto}";
+});
+
+/* Route::get('/redirect1', function(){
+    return redirect('/redirect2');
+}); */
+
+Route::redirect('/redirect1', '/redirect2');
+
+Route::get('/redirect2', function(){
+    return 'Redirect 2';
+});
+
+/* Route::get('/view', function(){
+    return view('sobre');
+}); */
+
+Route::view('/view', 'sobre');
+
+/* Rotas nomeadas */
+Route::get('/url-quem-somos', function(){
+    return '12345678';
+})->name('quem.somos');
+
+Route::get('/redirect3', function(){
+    return redirect()->route('quem.somos');
+});
